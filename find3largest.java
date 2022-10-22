2e50ae1645a015665d7f28d82a0b2244278cd9ea
kashish0604 <ustad.kashish@gmail.com>
2022-10-23 00:58:35 +0530

    alternate sort and 3rd lagest java done

--- find3largest.java
+++ find3largest.java
@@ -1,41 +1,23 @@
-public class first3largest {
-
-    // finding a largest number using recursive function
-
-    public static int findlargest(int[] arr, int i, int n) {
-        if (i == n - 1) {
-            return arr[i];
+public class New {
+    private static int findThirdLargest(int[] array) {
+        int first, second, third;
+        first = second = third = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > first) {
+                third = second;
+                second = first;
+                first = array[i];
+            } else if (array[i] > second) {
+                third = second;
+                second = array[i];
+            } else if (array[i] > third) {
+                third = array[i];
+            }
         }
-
-        int largest = findlargest(arr, i + 1, n);
-
-        return Math.max(arr[i], largest);
+        return third;
     }
-
-    public static void firstthreelargest(int[] arr){
-    int largest,secondlargest,thirdlargest;
-    largest=secondlargest=thirdlargest=Integer.MIN_VALUE;
-    for(int i=0;i<arr.length;i++){
-        if(arr[i]>largest){
-            thirdlargest=secondlargest;
-            secondlargest=largest;
-            largest=arr[i];
-        }
-        else if(arr[i]>secondlargest){
-            thirdlargest=secondlargest;
-            secondlargest=arr[i];
-        }
-        else if(arr[i]>thirdlargest){
-            thirdlargest=arr[i];
-        }
-    }    
-System.out.println(" "+largest+" "+secondlargest+" "+thirdlargest);
-
-    }
-
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 3, 50, 90, 90 };
-        firstthreelargest(arr);
+        int a[] = {4, 3, 2, 11, 23, 3, 44, 8, 93, 2, 34, 7, 8, 9};
+        System.out.println("Third largest number is " + findThirdLargest(a));
     }
-
 }
